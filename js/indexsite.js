061fcd1ea8c9561d7316122af9490647359c2fe2
andrea-kozicki <andrea.kozicki@gmail.com>
2025-03-26 22:11:57 -0300

    Adicionando arquivo js para referenciar o index.html

---
+++ js/indexsite.js
@@ -0,0 +1,88 @@
+/* Inicio Dropdown Navbar */
+let dropdownContainer = document.querySelector(".dropdown-container");
+let avatar = document.querySelector(".avatar");
+
+dropMenu(avatar);
+dropMenu(dropdownContainer);
+
+function dropMenu(selector) {
+    //console.log(selector);
+    selector.addEventListener("click", () => {
+        let dropdownMenu = selector.querySelector(".dropdown-menu");
+        dropdownMenu.classList.contains("active") ? dropdownMenu.classList.remove("active") : dropdownMenu.classList.add("active");
+    });
+}
+/* Fim Dropdown Navbar */
+
+/* Inicio Sidebar Toggle / bars */
+let sidebar = document.querySelector(".sidebar");
+let bars = document.querySelector(".bars");
+
+bars.addEventListener("click", () => {
+    sidebar.classList.contains("active") ? sidebar.classList.remove("active") : sidebar.classList.add("active");
+});
+
+window.matchMedia("(max-width: 768px)").matches ? sidebar.classList.remove("active") : sidebar.classList.add("active");
+/* Fim Sidebar Toggle / bars */
+
+function actionDropdown(id) {
+    closeDropdownAction();
+    document.getElementById("actionDropdown" + id).classList.toggle("show-dropdown-action");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches(".dropdown-btn-action")) {
+        /*document.getElementById("actionDropdown").classList.remove("show-dropdown-action");*/
+        closeDropdownAction();
+    }
+}
+
+function closeDropdownAction() {
+    var dropdowns = document.getElementsByClassName("dropdown-action-item");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i]
+        if (openDropdown.classList.contains("show-dropdown-action")) {
+            openDropdown.classList.remove("show-dropdown-action");
+        }
+    }
+}
+
+
+/* Inicio dropdown sidebar */
+
+var dropdownSidebar = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdownSidebar.length; i++) {
+    dropdownSidebar[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
+/* Fim dropdown sidebar */
+
+/* carrossel */
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval (function() {
+    nextImage();
+}, 4000)
+
+function nextImage(){
+    count++;
+    if(count > 5) {
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
+
+/*fim carrossel */
