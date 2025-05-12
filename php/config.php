854a8290ebf0ea3e00116a293d0960209588e83b
andrea-kozicki <72393301+andrea-kozicki@users.noreply.github.com>
2025-05-12 19:18:02 -0300

    Update config.php
    mudando o código para não deixar dados sensíveis expostos.

--- php/config.php
+++ php/config.php
@@ -1,6 +1,12 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 function getDatabaseConnection(): PDO {
-    return new PDO('mysql:host=localhost;dbname=login;charset=utf8mb4', 'andrea', 'v%PtYW5o9@y@c', [
+    $dsn = "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']};charset=utf8mb4";
+    return new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 }
